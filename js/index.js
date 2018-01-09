63d8dca881880c9f429261bbff7760d99998109f
Rupayan Neogy <rneogy@mit.edu>
2018-01-09 03:49:53 -0500

    complete

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ const user = {
 };
 
 renderNavbar(user);
+
+const postButton = document.getElementById('new-status-btn');
+postButton.addEventListener('click', function(){
+	const textInput = document.getElementById('new-status-text');
+	const newStatus = textInput.value;
+	const storiesDiv = document.getElementById('stories');
+
+	const newStory = document.createElement('div');
+	newStory.innerText = newStatus;
+	storiesDiv.appendChild(newStory);
+	
+	textInput.value = '';
+});
