69ac7f3e655f2442d46ad1be7051cb0fe21ba264
Rupayan Neogy <rneogy@mit.edu>
2018-01-09 02:41:54 -0500

    ex2 done

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,8 @@ let user = {
 };
 
 function renderUserData(aUser){
-
+	document.getElementById('profile-name').innerText = aUser.name;
+	document.getElementById('profile-status').innerText = aUser.last_post;
 }
 
 renderUserData(user);
