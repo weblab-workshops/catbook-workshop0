cc98c1e072fdb5cfa25dc4b0eb743a0d7d8a3b01
Rupayan Neogy <rneogy@mit.edu>
2018-01-09 02:57:09 -0500

    ex3 end

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,12 @@
 function newNavbarItem(text, url) {
 	// create an <a> element with text inside it, linking to url, and return the element
+	// also need to add the bootstrap classes "nav-item" and "nav-link"
+	const navItem = document.createElement('a');
+	navItem.className = "nav-item nav-link";
+	navItem.innerHTML = text;
+	navItem.href = url;
+
+	return navItem;
 }
 
 function renderNavbar(user) {
